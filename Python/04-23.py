e95bcb450389dd97a813540c8c7b07e10d023c00
dahyun <tjekgus609@gmail.com>
2021-04-23 22:04:06 +0900

    짝수 어쩌고

---
+++ Python/04-23.py
@@ -0,0 +1,12 @@
+def sol(s):
+    a = len(s)
+    answer = 0
+    for i in s:
+        if a%2 == 1:
+            
+                                      
+return 
+
+
+s = input()
+print(sol(s))
---
+++ Python/04-23_list.py
@@ -0,0 +1,15 @@
+# #점프 투 파이선:리스트
+##리스트의 사용방법
+# odd = [1, 3, 5, 7, 9]
+# print(odd)
+
+a = []
+print(a)
+b =[1, 2, 3]
+print(b)
+c = ['Neo','Technology','City','NCT']
+print(c)
+d =[1, 2, 'nct','dream']
+print(d)
+e = [1, 2, ['nct','dream']]
+print(e)
