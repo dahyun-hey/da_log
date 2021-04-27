8fb4b5a8f8ff2dba6d9893ec6d75418655319330
dahyun <tjekgus609@gmail.com>
2021-04-27 21:54:28 +0900

    프로그래머스 따릉

--- Python/04-23.py
+++ Python/04-23.py
@@ -1,12 +1,13 @@
-def sol(s):
-    a = len(s)
-    answer = 0
-    for i in s:
-        if a%2 == 1:
-            
-                                      
-return 
+#프로그래머스: 가운데 글자 가져오기
+#단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
 
+def solution(s):
+    for i in range(len(s)+1):
+        if len(s)%2 == 0:
+            a = s[int(len(s)/2)-1] + s[int(len(s)/2)]
+        elif len(s)%2 == 1:
+            a = s[int(len(s)/2+0.5)-1]
+    return a
 
-s = input()
-print(sol(s))
+s = "asdfasdf"
+print(solution(s))
