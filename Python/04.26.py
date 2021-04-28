a7e85fa2952ac0446531409c04a7fd6c55cd2064
dahyun <tjekgus609@gmail.com>
2021-04-28 13:24:20 +0900

    new file for Beakjoon

--- Python/04.26.py
+++
@@ -1,8 +0,0 @@
-def solution(arr):
-    b = 0
-    for i in arr:
-        b += i
-    return b 
-
-arr = [1,2,3,4,5]
-print(solution(arr))
--- Python/04.26team.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-x1_name = "log.xlsx"
-api_name = "log_api.csv"
-
-x1 = pd.read_excel(x1_name)
-api = pd.read_csv(api_name, encoding = 'cp949')
-
