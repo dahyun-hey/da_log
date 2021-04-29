09f0745beba8d4bbb1fecc0ae8e2782d7acf04b6
dahyun <tjekgus609@gmail.com>
2021-04-29 17:40:53 +0900

    확인

--- Python/programers/04-28_Dotproduct.py
+++ Python/programers/04-28_Dotproduct.py
@@ -1,8 +1,15 @@
-a = [1,2,3,4]
-b = [0, 0,0,0]
 
-c = 0
-for i,j in zip(range(len(a)+1),range(len(b)+1)):
-    c = a[i-1] * b[j-1]
-    c+=c
-print(c)
+a = [1,2,3,4]
+b = [-1,-2,0,0]
+
+# c = 0
+# for i,j in zip(range(len(a)+1),range(len(b)+1)):
+#     c = a[i-1] * b[j-1]
+#     c+=c
+# print(c)
+
+answer = 0
+for i in range(len(a)):
+    i = a[i]*b[i]
+    answer += i
+print(answer)
